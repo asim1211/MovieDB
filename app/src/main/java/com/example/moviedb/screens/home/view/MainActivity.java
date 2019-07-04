f367e3d515b25ef65880e3e0b0108861c31ddf96
asim hakami <asimhakami19@gmail.com>
2019-07-04 12:12:49 +0300

    fixing

--- app/src/main/java/com/example/moviedb/screens/home/view/MainActivity.java
+++ app/src/main/java/com/example/moviedb/screens/home/view/MainActivity.java
@@ -16,6 +16,8 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.example.moviedb.Genre;
+import com.example.moviedb.GenreJSONResults;
+import com.example.moviedb.JSONResult;
 import com.example.moviedb.Movie;
 import com.example.moviedb.screens.home.adapter.MovieAdapter;
 import com.example.moviedb.screens.home.presenter.MoviePresenter;
@@ -24,22 +26,25 @@ import com.example.moviedb.R;
 import com.example.moviedb.RetrofitClientInstance;
 import com.example.moviedb.screens.home.MoviesView;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
-<<<<<<< Updated upstream:app/src/main/java/com/example/moviedb/MainActivity.java
+
 public class MainActivity extends AppCompatActivity {
-=======
+
 public class MainActivity extends AppCompatActivity implements MoviesView.View {
->>>>>>> Stashed changes:app/src/main/java/com/example/moviedb/screens/home/view/MainActivity.java
+
 
     private Spinner spinner1;
     private MovieAdapter adapter;
     private RecyclerView recyclerView;
-    public static String key = "6a454f1310829848d7744fcda5a5cb30";
-<<<<<<< Updated upstream:app/src/main/java/com/example/moviedb/MainActivity.java
+    public String key = "6a454f1310829848d7744fcda5a5cb30";
+
     Call<GenreJSONResults> genreCall;
     List<Genre> genreList = new ArrayList<>();
     JSONResult movieList;
-=======
+
     private MoviePresenter presenter;
 
     @Override
@@ -80,95 +85,39 @@ public class MainActivity extends AppCompatActivity implements MoviesView.View {
         }
         adapter.updateData(moviesList);
     }
->>>>>>> Stashed changes:app/src/main/java/com/example/moviedb/screens/home/view/MainActivity.java
 
     class CustomOnItemSelectedListener implements AdapterView.OnItemSelectedListener {
 
-        public void onItemSelected(AdapterView<?> parent, View view, int pos,long id) {
+        public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
 
             ((TextView) parent.getChildAt(0)).setTextColor(Color.WHITE);
-<<<<<<< Updated upstream:app/src/main/java/com/example/moviedb/MainActivity.java
 
-            if (parent.getItemAtPosition(pos).toString().equals(genreList.get(pos).getName())){
+            if (parent.getItemAtPosition(pos).toString().equals(genreList.get(pos).getName())) {
                 gener = Integer.parseInt(genreList.get(pos).getId());
-=======
-            if (parent.getItemAtPosition(pos).toString().equals(genreList.get(pos).getName())){
-                presenter.gener = Integer.parseInt(genreList.get(pos).getId());
->>>>>>> Stashed changes:app/src/main/java/com/example/moviedb/screens/home/view/MainActivity.java
-            }else{
-                presenter.gener = 28;
+
+                if (parent.getItemAtPosition(pos).toString().equals(genreList.get(pos).getName())) {
+                    presenter.gener = Integer.parseInt(genreList.get(pos).getId());
+                } else {
+                    presenter.gener = 28;
+                }
+
+
+                presenter.getMovies();
+
+                presenter.getMovies(presenter.getCurrentGenre());
+
             }
 
-<<<<<<< Updated upstream:app/src/main/java/com/example/moviedb/MainActivity.java
-            getMovies();
-=======
-            //getMovies();
-            presenter.getMovies(presenter.getCurrentGenre());
->>>>>>> Stashed changes:app/src/main/java/com/example/moviedb/screens/home/view/MainActivity.java
-        }
 
-        @Override
-        public void onNothingSelected(AdapterView<?> arg0) {
-            // TODO Auto-generated method stub
         }
 
     }
 
-<<<<<<< Updated upstream:app/src/main/java/com/example/moviedb/MainActivity.java
-    private void populateListView(List<Movie> moviesList) {
-        recyclerView = (RecyclerView) findViewById(R.id.listings_view);
-        adapter = new MovieAdapter(this,moviesList);
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        addListenerOnSpinnerItemSelection();
-
-        getGenre();
-        getMovies();
-
-    }
-
-    // should be in the presenter
-    private void getGenre() {
-       // genreCall = myInterface.getGenre(key);
-        RetrofitClientInstance.getInstance().getGenres(new Callback<GenreJSONResults>() {
-            @Override
-            public void onResponse(Call<GenreJSONResults> genreCall, Response<GenreJSONResults> response) {
-
-                GenreJSONResults list = response.body();
-                List<Genre> g = list.getGenres();
-                genreList.addAll(g);
-                addItemsOnSpinner(g);
-            }
-
-            @Override
-            public void onFailure(Call<GenreJSONResults> genreCall, Throwable throwable) {
-            }
-        });
-    }
 
 
-        // should be in the presenter
-    private void getMovies() {
-        RetrofitClientInstance.getInstance().getMovies(gener, new Callback<JSONResult>() {
-            @Override
-            public void onResponse(Call<JSONResult>call, Response<JSONResult> response) {
-                movieList = response.body();
-                List<Movie> m = movieList.getMovies();
-                populateListView(m);
 
-            }
 
-            @Override
-            public void onFailure(Call<JSONResult> call, Throwable throwable) {
-            }
-        });
-    }
+
 
 
     // add items into spinner dynamically
@@ -186,8 +135,7 @@ public class MainActivity extends AppCompatActivity implements MoviesView.View {
     }
 
 
-=======
->>>>>>> Stashed changes:app/src/main/java/com/example/moviedb/screens/home/view/MainActivity.java
+
     // changing the spinner dynamically
     public void addListenerOnSpinnerItemSelection() {
         spinner1 = (Spinner) findViewById(R.id.spinner1);
